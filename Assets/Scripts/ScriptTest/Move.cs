0405198e68ff75b052d42df76825c1231d45ca50
arco03 <dmglux13@gmail.com>
2024-05-16 14:39:39 -0500

    Primera parte del movimiento
    Se creo la parte inicial de movimiento.

---
+++ Assets/Scripts/ScriptTest/Move.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace ScriptTest
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Move : MonoBehaviour
+    {
+        [SerializeField] private string horizontal;
+        [SerializeField] private string vertical;
+        [SerializeField] private float speed;
+        private Rigidbody _rb;
+        private float _x, _y;
+        
+        
+        private void Update()
+        {
+            _x = Input.GetAxisRaw(horizontal);
+            _y = Input.GetAxisRaw(vertical);
+            
+            
+        }
+
+        private void FixedUpdate()
+        {
+            transform.Translate(_x*speed,0f,_y*speed);
+        }
+        
+    }
+}

b6efea776a613eb05aefd54cf65dbcdc35ff4b1e
DianaMontoya19 <153123795+DianaMontoya19@users.noreply.github.com>
2024-05-17 23:19:23 -0500

    Scriptable

--- Assets/Scripts/Scriptable/IngredientsData.cs
+++ Assets/Scripts/Scriptable/IngredientsData.cs
@@ -1,5 +1,6 @@
 ﻿using Interactable;
 using Playable;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Scriptable
@@ -7,9 +8,13 @@ namespace Scriptable
     [CreateAssetMenu(menuName = "Create Ingredients", fileName = "Ingredients")]
     public class IngredientsData : ScriptableObject
     {
-        public int id;
-        public string ingredientName;
-        public Sprite sprite;
-        public Ingredient ingredientPrefab; //Aun no estoy segura del tipo de dato 
+        [HideInInspector] public int id;
+
+        public List<GameObject> prefabs;
+
+       
+        //public string ingredientName;
+        //public Sprite sprite;
+        //public Ingredient ingredientPrefab; //Aun no estoy segura del tipo de dato 
     }
 }

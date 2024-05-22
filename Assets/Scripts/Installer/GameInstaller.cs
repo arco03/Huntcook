b9c0d41696249be58a40930b5802d0bea4bce309
arco03 <dmglux13@gmail.com>
2024-05-22 14:19:34 -0500

    Spawner
    Se creo una forma de spawnear los enemigos y la comida de manera aleatoria

--- Assets/Scripts/Installer/GameInstaller.cs
+++ Assets/Scripts/Installer/GameInstaller.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Interactable;
 using Playable;
 using Scriptable;
@@ -8,14 +10,21 @@ using Random = UnityEngine.Random;
 
 namespace Installer
 {
+    [RequireComponent(typeof(Transform))]
     public class GameInstaller : MonoBehaviour
     {
+        [Header("Spawner configurations")]
         [SerializeField] private ListGhost config;
         [SerializeField] private IngredientsData configFood;
         
+        [Header("Spawner Positions")]
+        [SerializeField] private Transform ghostVector1;
+        [SerializeField] private Transform ghostVector2;
+        [SerializeField] private List<Transform> ingredientsSpawnPoints;
+        
         private ConfigureFactory _enemy;
         private FactoryFood _food;
-        
+
         public int id;
 
         public void Awake()
@@ -25,30 +34,45 @@ namespace Installer
 
         }
 
-        public void Start()
+        private void Start()
         {
-            for(int i = 0; i < 3; i++)
-            { 
-                float posX = Random.Range(-5, 5);
-                float posZ = Random.Range(-3, 8);
+            StartCoroutine(Timer());
+
+        }
+        
+
+        public void Spawners()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                float posEnemyX = Random.Range(Mathf.Max(ghostVector1.position.x, ghostVector2.position.x),Mathf.Min(ghostVector1.position.x, ghostVector2.position.x));
+                float posEnemyZ = Random.Range(Mathf.Max(ghostVector1.position.z, ghostVector2.position.z), Mathf.Min(ghostVector1.position.z, ghostVector2.position.z));
+                
+                int randomIndex = Random.Range(0, ingredientsSpawnPoints.Count);
+                
+                Transform randomSpawnPoint = ingredientsSpawnPoints[randomIndex];
                 
                 GhostGame enemy = _enemy.Create(0);
                 Ingredient food = _food.Create(i);
+
+                food.transform.position = new Vector3(randomSpawnPoint.position.x,randomSpawnPoint.position.y,randomSpawnPoint.position.z);
+                enemy.transform.position = new Vector3(posEnemyX, 1f, posEnemyZ);
                 
-                food.transform.position = new Vector3(6.57000017f,1.71200001f ,0f+i * 2);
-                enemy.transform.position = new Vector3(posX , 1f, posZ );
-
+                
+                
+                //float posFoodX = Random.Range(Mathf.Max(ingredientsVector1.position.x,ingredientsVector2.position.x),Mathf.Min(ingredientsVector1.position.x,ingredientsVector2.position.x));
+               //float posFoodZ = Random.Range(Mathf.Max(ingredientsVector1.position.z,ingredientsVector2.position.z),Mathf.Min(ingredientsVector1.position.z,ingredientsVector2.position.z));
                 // enemy.AddIngredient(food);
-
             }
+        }
+        IEnumerator Timer()
+        {
             
-            
-
-
-
-            
-         
+            yield return new WaitForSeconds(5f);
+            Spawners();
             
         }
     }
 }
+
+

bf8af0dc2390f53e55260d3d25d5206cdff59735
arco03 <dmglux13@gmail.com>
2024-05-29 11:49:04 -0500

    Update; UI scripts

---
+++ Assets/_Scripts/UI/UIController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _Scripts.UI
+{
+    public class UIController : MonoBehaviour
+    {
+        [SerializeField] private UIView uiView;
+        
+    }
+}

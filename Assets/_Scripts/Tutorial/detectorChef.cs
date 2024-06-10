c6aae2ca736740747ecb427faa9bf24ba178d703
DianaMontoya19 <153123795+DianaMontoya19@users.noreply.github.com>
2024-06-09 21:28:48 -0500

    tutorial

---
+++ Assets/_Scripts/Tutorial/detectorChef.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using _Scripts.Player;
+using UnityEngine;
+
+namespace _Scripts.Tutorial
+{
+    public class detectorChef : MonoBehaviour
+    {
+        public bool detector;
+        public bool detectorGhost;
+        public bool detectorPlate;
+        
+
+        
+
+        public void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.CompareTag("Ingredients"))
+            {
+                detector = true;
+            }
+
+            if (other.gameObject.CompareTag("Ghost"))
+            {   
+               detectorGhost = true;
+            }
+
+            if (other.gameObject.CompareTag("DishPoint"))
+            {
+                detectorPlate = true;
+
+            }
+        }
+    }
+}

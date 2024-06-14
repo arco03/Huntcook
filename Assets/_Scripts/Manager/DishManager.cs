d41dd9282084a3371098e9d2d8bde12b0e937205
arco03 <dmglux13@gmail.com>
2024-06-14 01:41:30 -0500

    Cambios finales UI

--- Assets/_Scripts/Manager/DishManager.cs
+++ Assets/_Scripts/Manager/DishManager.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using _Scripts.Audio;
 using _Scripts.Dish;
 using _Scripts.Ingredient;
+using _Scripts.UI.Ingredient;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _Scripts.Manager
 {
@@ -28,21 +30,21 @@ namespace _Scripts.Manager
         [SerializeField] private IngredientConfiguration ingredientConfiguration;
         private IngredientSpawner _ingredientSpawner;
         public List<IngredientPoint> ingredientPoints;
+        [SerializeField] private IngredientController ingredientController;
 
         [SerializeField] private Light lightPoint;
         [SerializeField] private Color readyColor;
-        [SerializeField] private string nameSound;
 
         private Transform _transform;
-        public int count;
+        public int count = 0;
         public int index;
         public int level;
-
+        public string soundName;
 
         private void Awake()
         {
+            
             OnDishReady += HandleDishReady;
-
             DishFactory dishFactory = new DishFactory(dishConfiguration);
             _dishSpawner = new DishSpawner(dishFactory);
 
@@ -57,11 +59,19 @@ namespace _Scripts.Manager
             uiManager.UpdateDish(data[index]);
         }
 
+
+
         private void TypeLevel(int level)
         {
-            for (int i = 0; i < level; i++)
+            int minAmount = 1; // Mínimo valor posible para el amount
+            int maxAmount = 6; // Máximo valor posible para el amount
+            int initialAmount = Random.Range(minAmount, maxAmount + 1); 
+            
+            for (int i = 0; i < level && i < data.Length; i++)
             {
+                data[i].amount = initialAmount;
                 _ingredientSpawner.Initialize(data[i].ingredientsList);
+                
             }
         }
         
@@ -80,32 +90,45 @@ namespace _Scripts.Manager
 
         private void HandleDishReady(DishData dataDish)
         {
-            if (count <= data[index].amount)
+            if (index >= data.Length)
+                return;
+            while (count <= data[index].amount)
             {
-                count++;
+                data[index].amount--;
                 uiManager.UpdateDish(data[index]);
-                Debug.Log("Plato List");
+                 
+                
+                Debug.Log($"Plato List {data[index].amount}");
+                AudioManager.instance.PlaySfx(soundName);
                 StartCoroutine(ChangeLightColorTemporarily());
-                AudioManager.instance.PlaySfx(nameSound);
                 StartCoroutine(TimeReset());
+                
+                Debug.Log($"Count {count}");
+
+                // Esperar un frame antes de continuar con el bucle
+                if (count < data[index].amount)
+                {
+                    return;
+                }
+                
+                count++;
+                Debug.Log($"Index {index}");
+                Debug.Log($"Data Index {data[index]}");
             }
 
-            if (count == data[index].amount)
+            if (count > data[index].amount)
             {
                 index++;
                 count = 0;
-                StartCoroutine(TimeReset());
-
+               
+                if (index < data.Length)
+                    uiManager.UpdateDish(data[index]);
                 if (index >= data.Length)
-                {
                     OnDishComplete?.Invoke();
-                    return;
-                }
-                uiManager.UpdateDish(data[index]);
+
             }
-            Debug.Log($"Count {count}");
-            Debug.Log($"Index {index}");
-            Debug.Log($"Data Index {data[index]}");
+            // uiManager.UpdateDish(data[index])
+
         }
 
         private IEnumerator TimeReset()

be2db725b2f105bcd25667940f0a4edbad980c1f
DianaMontoya19 <dicaro2001@hotmail.com>
2024-07-11 00:31:42 -0500

    Menu_Pause

---
+++ Assets/_Scripts/UI/Menu/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+namespace _Scripts.UI.Menu
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject menuPause;
+        [SerializeField] public GameObject pause;
+        
+        public void Pause()
+        {
+            Time.timeScale = 0f;
+            pause.SetActive(false);
+            menuPause.SetActive(true);
+        }
+        public void Resume()
+        {
+            Time.timeScale = 1f;
+            pause.SetActive(true);
+            menuPause.SetActive(false);
+        }
+        public void Restart()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
